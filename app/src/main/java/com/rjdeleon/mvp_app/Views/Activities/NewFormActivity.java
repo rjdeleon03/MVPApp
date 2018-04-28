c675410716c2f9d3a2458d053a7b297692117f3f
rjdeleon <ricardopablo.deleon@gmail.com>
2018-04-28 10:46:56 +0800

    Add new DNCA form activity

---
+++ app/src/main/java/com/rjdeleon/mvp_app/Views/Activities/NewFormActivity.java
@@ -0,0 +1,13 @@
+package com.rjdeleon.mvp_app.Views.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_form_activity);
+    }
+}

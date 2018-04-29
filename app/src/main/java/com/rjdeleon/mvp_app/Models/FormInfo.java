b297a726c912a16b3b57ef659590b59a8417d2e9
rjdeleon <ricardopablo.deleon@gmail.com>
2018-04-29 16:20:16 +0800

    Support databinding to model in form details fragment

--- app/src/main/java/com/rjdeleon/mvp_app/Models/FormInfo.java
+++ app/src/main/java/com/rjdeleon/mvp_app/Models/FormInfo.java
@@ -1,8 +1,11 @@
 package com.rjdeleon.mvp_app.Models;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
 import com.rjdeleon.mvp_app.Models.GeneralInformation.GenInfo;
 
-public class FormInfo {
+public class FormInfo extends BaseObservable {
     private String orgName;
     private String assessmentDate;
     private String sitio;
@@ -25,39 +28,84 @@ public class FormInfo {
         this.genInfo = genInfo;
     }
 
+    @Bindable
     public String getOrgName() {
         return orgName;
     }
 
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    @Bindable
     public String getAssessmentDate() {
         return assessmentDate;
     }
 
+    public void setAssessmentDate(String assessmentDate) {
+        this.assessmentDate = assessmentDate;
+    }
+
+    @Bindable
     public String getSitio() {
         return sitio;
     }
 
+    public void setSitio(String sitio) {
+        this.sitio = sitio;
+    }
+
+    @Bindable
     public String getBarangay() {
         return barangay;
     }
 
+    public void setBarangay(String barangay) {
+        this.barangay = barangay;
+    }
+
+    @Bindable
     public String getCity() {
         return city;
     }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Bindable
     public String getProvince() {
         return province;
     }
 
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    @Bindable
     public String getInterviewer() {
         return interviewer;
     }
 
+    public void setInterviewer(String interviewer) {
+        this.interviewer = interviewer;
+    }
+
+    @Bindable
     public String getInterviewerNo() {
         return interviewerNo;
     }
 
+    public void setInterviewerNo(String interviewerNo) {
+        this.interviewerNo = interviewerNo;
+    }
+
+    @Bindable
     public GenInfo getGenInfo() {
         return genInfo;
+    }
+
+    public void setGenInfo(GenInfo genInfo) {
+        this.genInfo = genInfo;
     }
 }
--- app/src/main/java/com/rjdeleon/mvp_app/Views/Fragments/NewFormDetailsFragment.java
+++ app/src/main/java/com/rjdeleon/mvp_app/Views/Fragments/NewFormDetailsFragment.java
@@ -1,52 +1,43 @@
 package com.rjdeleon.mvp_app.Views.Fragments;
 
-import android.net.Uri;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.rjdeleon.mvp_app.Models.FormInfo;
+import com.rjdeleon.mvp_app.Models.GenderTuple;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.CalamityDesc;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.CasualtiesData;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.CasualtiesDataRow;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.DeathCauseData;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.DeathCauseDataRow;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.FamilyData;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.GenInfo;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.InfrastructureDamageData;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.InfrastructureDamageDataRow;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.PopulationData;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.PopulationDataRow;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.VulnerablePopulationData;
+import com.rjdeleon.mvp_app.Models.GeneralInformation.VulnerablePopulationDataRow;
 import com.rjdeleon.mvp_app.R;
+import com.rjdeleon.mvp_app.databinding.NewFormDetailsFragmentBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NewFormDetailsFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NewFormDetailsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class NewFormDetailsFragment extends BaseFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private FormInfo mFormInfo;
 
     public NewFormDetailsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NewFormDetailsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NewFormDetailsFragment newInstance(String param1, String param2) {
+    public static NewFormDetailsFragment newInstance() {
         NewFormDetailsFragment fragment = new NewFormDetailsFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -54,38 +45,64 @@ public class NewFormDetailsFragment extends BaseFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        mFormInfo = seedData();
+        NewFormDetailsFragmentBinding binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.new_form_details_fragment, container, false);
+        binding.setFormInfo(mFormInfo);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.new_form_details_fragment, container, false);
+        return binding.getRoot();
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    /* SAMPLE SEED DATA */
+    private FormInfo seedData() {
+
+        List<PopulationDataRow> rows = new ArrayList<>();
+        List<VulnerablePopulationDataRow> vpRows = new ArrayList<>();
+        List<CasualtiesDataRow> cdRows = new ArrayList<>();
+        List<DeathCauseDataRow> dcdRows = new ArrayList<>();
+        List<InfrastructureDamageDataRow> iddRows = new ArrayList<>();
+
+        for (int i = 0; i < PopulationData.AgeGroup.values().length - 1; i++) {
+            PopulationData.AgeGroup ageGroup = PopulationData.AgeGroup.values()[i];
+            PopulationDataRow row = new PopulationDataRow(ageGroup,
+                    new GenderTuple(1,2),
+                    new GenderTuple(3,4),
+                    new GenderTuple(5,6));
+            rows.add(row);
+
+            GenderTuple gt = new GenderTuple(55, 66);
+
+            VulnerablePopulationDataRow vpRow = new VulnerablePopulationDataRow(ageGroup, 11, 22, 33, 44, gt, gt, gt, "vpRow");
+            vpRows.add(vpRow);
+
+            CasualtiesDataRow cdRow = new CasualtiesDataRow(ageGroup, gt, gt, gt);
+            cdRows.add(cdRow);
+
+            DeathCauseDataRow dcdRow = new DeathCauseDataRow(ageGroup, gt, gt, gt, gt, gt, gt, gt, gt, gt);
+            dcdRows.add(dcdRow);
         }
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        for (int i = 0; i < InfrastructureDamageData.InfraType.values().length; i++) {
+            InfrastructureDamageDataRow iddRow = new InfrastructureDamageDataRow(InfrastructureDamageData.InfraType.values()[i], 77, true, "damagedInfra");
+            iddRows.add(iddRow);
+        }
+
+        PopulationData populationData = new PopulationData(rows);
+        CalamityDesc calamityDesc = new CalamityDesc("xxx", "yyy", "zzz");
+        FamilyData familyData = new FamilyData(11, 22, 33, 44, 55, 66);
+        VulnerablePopulationData vulnerablePopulationData = new VulnerablePopulationData(vpRows);
+        CasualtiesData casualtiesData = new CasualtiesData(cdRows);
+        DeathCauseData deathCauseData = new DeathCauseData(dcdRows);
+        InfrastructureDamageData infrastructureDamageData = new InfrastructureDamageData(iddRows);
+
+        GenInfo genInfo = new GenInfo(calamityDesc, "affectedArea", populationData, familyData, vulnerablePopulationData, casualtiesData, deathCauseData, infrastructureDamageData);
+        return new FormInfo("aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", genInfo);
     }
+    /* ================ */
 }

4198cc91f3dadc99ee599e64bee4c41ea4b34fe7
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2018-06-17 10:34:01 +0800

    Add base code for interviewee details

---
+++ app/src/main/java/com/cpu/quikdata/Modules/NewDnca/FormDetails/InterviewDetails/InterviewDetailsItem/Dialog/InterviewDetailsDialogFragment.java
@@ -0,0 +1,4 @@
+package com.cpu.quikdata.Modules.NewDnca.FormDetails.InterviewDetails.InterviewDetailsItem.Dialog;
+
+public class InterviewDetailsDialogFragment {
+}
---
+++ app/src/main/java/com/cpu/quikdata/Modules/NewDnca/FormDetails/InterviewDetails/InterviewDetailsItem/Row/InterviewDetailsRowViewHolder.java
@@ -0,0 +1,4 @@
+package com.cpu.quikdata.Modules.NewDnca.FormDetails.InterviewDetails.InterviewDetailsItem.Row;
+
+public class InterviewDetailsRowViewHolder {
+}

dfb1e4a7094259bd706b2dfecd982e3bc2c36c87
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2018-06-17 09:20:21 +0800

    Add base code for form details

---
+++ app/src/main/java/com/cpu/quikdata/Modules/NewDnca/FormDetails/GenFormDetails/GenFormDetailsViewModel.java
@@ -0,0 +1,4 @@
+package com.cpu.quikdata.Modules.NewDnca.FormDetails.GenFormDetails;
+
+public class GenFormDetailsViewModel {
+}
---
+++ app/src/main/java/com/cpu/quikdata/Modules/NewDnca/FormDetails/InterviewDetails/InterviewDetailsViewModel.java
@@ -0,0 +1,4 @@
+package com.cpu.quikdata.Modules.NewDnca.FormDetails.InterviewDetails;
+
+public class InterviewDetailsViewModel {
+}

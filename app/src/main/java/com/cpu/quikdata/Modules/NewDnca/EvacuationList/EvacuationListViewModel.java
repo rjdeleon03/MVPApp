2176f7085c4714bf71dd49fc559dfac0bb6adba3
Ricardo Pablo De Leon <ricardopablo.deleon@gmail.com>
2018-06-29 22:34:00 +0800

    Remove unnecessary code

--- app/src/main/java/com/cpu/quikdata/Modules/NewDnca/EvacuationList/EvacuationListViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.cpu.quikdata.Modules.NewDnca.EvacuationList;
-
-import com.cpu.quikdata.Models.Evacuation.EvacuationInfo;
-import com.cpu.quikdata.Modules.NewDnca.Base.MultiPageFragment.BaseMultiPageViewModel;
-
-import java.util.List;
-
-public class EvacuationListViewModel {
-}

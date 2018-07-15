c516eecb6e1b6a5bc269e4c963fd2fa4c555ae9f
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-07-15 14:46:39 +0800

    Add calamity details fragment and viewmodel

--- app/src/main/java/com/cpu/quikdata/ModulesV2/NewForm/GeneralInformation/CalamityDetails/CalamityDetailsFragment.java
+++ app/src/main/java/com/cpu/quikdata/ModulesV2/NewForm/GeneralInformation/CalamityDetails/CalamityDetailsFragment.java
@@ -1,4 +1,19 @@
 package com.cpu.quikdata.ModulesV2.NewForm.GeneralInformation.CalamityDetails;
 
-public class CalamityDetailsFragment {
+import com.cpu.quikdata.ModulesV2.Base.MainTemplate.TemplateQuestionFragment;
+
+public class CalamityDetailsFragment extends TemplateQuestionFragment {
+
+    public CalamityDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Gets new instance
+     * @return
+     */
+    public static CalamityDetailsFragment newInstance() {
+
+        return new CalamityDetailsFragment();
+    }
 }
--- app/src/main/java/com/cpu/quikdata/ModulesV2/NewForm/GeneralInformation/CalamityDetails/CalamityDetailsViewModel.java
+++ app/src/main/java/com/cpu/quikdata/ModulesV2/NewForm/GeneralInformation/CalamityDetails/CalamityDetailsViewModel.java
@@ -1,4 +1,37 @@
 package com.cpu.quikdata.ModulesV2.NewForm.GeneralInformation.CalamityDetails;
 
-public class CalamityDetailsViewModel {
+import com.cpu.quikdata.Models.DNCAFormRepository;
+import com.cpu.quikdata.ModelsV2.Form.GeneralInformation.CalamityDetails;
+import com.cpu.quikdata.ModelsV2.Form.GeneralInformation.GeneralInformation;
+import com.cpu.quikdata.ModelsV2.Form.GeneralInformation.PopulationData;
+import com.cpu.quikdata.ModulesV2.Base.MainTemplate.Models.QuestionItemModelString;
+import com.cpu.quikdata.ModulesV2.Base.MainTemplate.ViewModel.TemplateQuestionViewModel;
+import com.cpu.quikdata.ModulesV2.NewForm.INewFormActivity;
+
+public class CalamityDetailsViewModel extends TemplateQuestionViewModel<INewFormActivity, GeneralInformation> {
+
+    private CalamityDetails mCalamityDetails;
+
+    /**
+     * Constructor
+     *
+     * @param dncaFormRepository
+     */
+    public CalamityDetailsViewModel(DNCAFormRepository dncaFormRepository) {
+        super(dncaFormRepository);
+        mFormRepository.getGeneralInformation(this);
+    }
+
+    /**
+     * Handles reception of general information data
+     * @param data
+     */
+    @Override
+    public void onDataReceived(GeneralInformation data) {
+        mCalamityDetails = data.getCalamityDetails();
+
+        mQuestions.add(new QuestionItemModelString("Calamity Type: ", mCalamityDetails.getCalamityType()));
+        mQuestions.add(new QuestionItemModelString("Event Description: ", mCalamityDetails.getEventDesc()));
+        mQuestions.add(new QuestionItemModelString("Area Description: ", mCalamityDetails.getAreaDesc()));
+    }
 }

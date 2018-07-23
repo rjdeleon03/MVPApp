4c051afce440d04370343a77f175f0c4e17eaf4a
pabs <pabs@alink-group.co.jp>
2018-07-24 07:34:07 +0800

    Add viewmodel and viewholder for double string question

---
+++ app/src/main/java/com/cpu/quikdata/ModulesV2/Base/MainTemplate/ItemViewModels/TemplateQuestionItemViewModelDoubleString.java
@@ -0,0 +1,40 @@
+package com.cpu.quikdata.ModulesV2.Base.MainTemplate.ItemViewModels;
+
+import android.databinding.ObservableField;
+
+import com.cpu.quikdata.AppConstants;
+import com.cpu.quikdata.ModulesV2.Base.MainTemplate.Models.QuestionItemModelDoubleString;
+import com.cpu.quikdata.ModulesV2.Base.MainTemplate.Models.QuestionItemModelString;
+
+public class TemplateQuestionItemViewModelDoubleString extends TemplateQuestionItemViewModel<QuestionItemModelDoubleString> {
+
+    public final ObservableField<String> subQuestion1 = new ObservableField<>("");
+    public final ObservableField<String> value1 = new ObservableField<>("");
+    public final ObservableField<String> subQuestion2 = new ObservableField<>("");
+    public final ObservableField<String> value2 = new ObservableField<>("");
+
+    public TemplateQuestionItemViewModelDoubleString(QuestionItemModelDoubleString model) {
+        super(model);
+        question.set(mModel.getQuestion());
+        subQuestion1.set(mModel.getSubQuestion1());
+        value1.set(mModel.getValue1());
+        subQuestion2.set(mModel.getSubQuestion2());
+        value2.set(mModel.getValue2());
+    }
+
+    @Override
+    public QuestionItemModelDoubleString getModel() {
+        return new QuestionItemModelDoubleString(question.get(), subQuestion1.get(), value1.get(), subQuestion2.get(), value2.get());
+    }
+
+    @Override
+    public AppConstants.QuestionItemType getType() {
+        return AppConstants.QuestionItemType.DOUBLE_STRING;
+    }
+
+    @Override
+    public void updateModel() {
+        mModel.setValue1(value1.get());
+        mModel.setValue2(value2.get());
+    }
+}

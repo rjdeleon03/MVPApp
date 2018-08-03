2ddcbfddeb6d25c44a12bfbc144337cc33f28bf1
pabs <pabs@alink-group.co.jp>
2018-08-03 10:33:51 +0800

    Add evacuation info padding

--- app/src/main/java/com/cpu/quikdata/ModulesV2/NewForm/EvacuationInformation/EvacuationInfoList/EvacuationInfoListFragment.java
+++ app/src/main/java/com/cpu/quikdata/ModulesV2/NewForm/EvacuationInformation/EvacuationInfoList/EvacuationInfoListFragment.java
@@ -1,5 +1,6 @@
 package com.cpu.quikdata.ModulesV2.NewForm.EvacuationInformation.EvacuationInfoList;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cpu.quikdata.AppUtil;
 import com.cpu.quikdata.ModulesV2.Base.BaseFragment;
 import com.cpu.quikdata.ModulesV2.Base.EnumData.ITemplateEnumDataFragment;
 import com.cpu.quikdata.ModulesV2.NewForm.EvacuationInformation.EvacuationInfoList.EvacuationItem.EvacuationItemFragment;
@@ -20,6 +22,7 @@ import com.cpu.quikdata.databinding.TemplateEnumDataFragmentBinding;
 public class EvacuationInfoListFragment extends BaseFragment<EvacuationInfoListViewModel> implements ITemplateEnumDataFragment {
 
     private int mContainerId;
+    private Context mContext;
 
     public EvacuationInfoListFragment() {
         // Required empty public constructor
@@ -39,9 +42,15 @@ public class EvacuationInfoListFragment extends BaseFragment<EvacuationInfoListV
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        mContext = getContext();
+
         View view =  inflater.inflate(R.layout.template_enum_data_fragment, container, false);
         RecyclerView recyclerView = view.findViewById(R.id.template_enum_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+
+        if (mContext != null) {
+            recyclerView.setPadding(recyclerView.getPaddingLeft(), AppUtil.dpToPx(mContext, 16), recyclerView.getPaddingRight(), recyclerView.getPaddingBottom());
+        }
 
         mContainerId = container.getId();
 
@@ -57,7 +66,7 @@ public class EvacuationInfoListFragment extends BaseFragment<EvacuationInfoListV
     public void onAddButtonPressed() {
         FragmentManager parentFragmentManager = getActivity().getSupportFragmentManager();
         EvacuationItemFragment evacuationItemFragment = (EvacuationItemFragment) ViewFactory.findOrCreateFragment(parentFragmentManager, NewFormActivity.NewFormComponent.EVACUATION_ITEM, mContainerId);
-        final EvacuationItemViewModel evacuationItemViewModel = (EvacuationItemViewModel) ViewFactory.findOrCreateViewModel(parentFragmentManager, NewFormActivity.NewFormComponent.EVACUATION_ITEM, this, getContext());
+        final EvacuationItemViewModel evacuationItemViewModel = (EvacuationItemViewModel) ViewFactory.findOrCreateViewModel(parentFragmentManager, NewFormActivity.NewFormComponent.EVACUATION_ITEM, this, mContext);
         evacuationItemViewModel.setDataManager(mViewModel);
         evacuationItemFragment.setViewModel(evacuationItemViewModel);
     }
@@ -69,7 +78,7 @@ public class EvacuationInfoListFragment extends BaseFragment<EvacuationInfoListV
     public void onCardSelected(int index) {
         FragmentManager parentFragmentManager = getActivity().getSupportFragmentManager();
         EvacuationItemFragment evacuationItemFragment = (EvacuationItemFragment) ViewFactory.findOrCreateFragment(parentFragmentManager, NewFormActivity.NewFormComponent.EVACUATION_ITEM, mContainerId);
-        final EvacuationItemViewModel evacuationItemViewModel = (EvacuationItemViewModel) ViewFactory.findOrCreateViewModel(parentFragmentManager, NewFormActivity.NewFormComponent.EVACUATION_ITEM, this, getContext());
+        final EvacuationItemViewModel evacuationItemViewModel = (EvacuationItemViewModel) ViewFactory.findOrCreateViewModel(parentFragmentManager, NewFormActivity.NewFormComponent.EVACUATION_ITEM, this, mContext);
         evacuationItemViewModel.setDataManager(mViewModel, index);
         evacuationItemFragment.setViewModel(evacuationItemViewModel);
     }
